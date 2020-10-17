099181d9d2706c5a5e129ea21061041df957ee25
Kyle Smith <Kyle.Smith1@Nintex.com>
2020-10-17 11:23:25 +1100

    Comment updates

--- src/Get-UtMasterServerIpAddressList.ps1
+++ src/Get-UtMasterServerIpAddressList.ps1
@@ -8,15 +8,15 @@ function Get-UtMasterServerEndpointList {
         When a UT Game server is started it might say:
         UdpServerUplink: Master Server is master.mplayer.com:27900
         UdpServerUplink: Port 7779 successfully bound.
-        However, this just means that it was able to a) resolve the dns host name to an ip address and b) bind to the port 7779 on the system. 
+        However, this just means that it was able to a) resolve the dns host name to an ip address and b) bind to port 7779 on the system. 
         It does not mean the game server successfully announced its presense to that master server. 
 
-        Another problem is the UT99 Game client, will only show a list of servers in the browser that it managed to ping, 
+        Another problem is the UT99 Game client, will only show a list of servers in the browser that it managed to ping,
         so, you will not know if the problem is between the client and the game server, or the game server and the master server.
 
         This cmdlet allows you to test two things
         1. The master server address and port number are responding to connections.
-        2. The list of IP addresses is retrievable and your game server's ip address is on that list.
+        2. The list of IP Endpoints is retrievable and your game server's IP Endpoint is among them.
 
     .EXAMPLE
         PS C:\> Get-UtMasterServerEndpointList -Address utmaster.epicgames.com 
