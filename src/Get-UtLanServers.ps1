365ca803796ec8af05f0f56ba38287e8bff6d1cd
Kyle Smith <Kyle.Smith1@Nintex.com>
2020-10-17 16:30:05 +1100

    working multiple udp servers

--- src/Get-UtLanServers.ps1
+++ src/Get-UtLanServers.ps1
@@ -1,28 +1,36 @@
-#region function defs
-
-
 function Find-UtLanServers {
     try {
-        $ServerEndpoint = [IPEndpoint]::new([ipaddress]::Any, 0)
+        
         $udpClient = New-Object System.Net.Sockets.UdpClient
         $udpClient.EnableBroadcast = $true
-        $udpClient.Client.ReceiveTimeout = 1000
+        $udpClient.Client.ReceiveTimeout = 2000
+        $udpClient.Client.SendTimeout = 2000
         $encoding = [System.Text.AsciiEncoding]::new()
     
         #fishing for servers on the network.
         [Byte[]] $sendQueryBytes = $encoding.GetBytes("REPORTQUERY");
         8777..8786 | ForEach-Object { $udpClient.Send($sendQueryBytes, $sendQueryBytes.Length, [IPEndPoint]::new([IPAddress]::Broadcast, $_)) | Out-Null }
         
-        [Byte[]] $receiveBytes = $udpClient.Receive([ref]$ServerEndpoint);
-        [string] $returnData = $encoding.GetString($receiveBytes);
+        #new Testing
+        <# [System.AsyncCallback]::new($recv)
+        $udpClient.BeginReceive($recv, $state) #>
+        while ($true) {
+            $ServerEndpoint = [IPEndpoint]::new([ipaddress]::Any, 0)
+            [Byte[]] $receiveBytes = $udpClient.Receive([ref]$ServerEndpoint)
+            [string] $returnData = $encoding.GetString($receiveBytes);
     
-        if ($returnData) {
-            $output = [PSCustomObject]@{
-                GameType = $returnData.Split(' ')[0]
-                Port     = $returnData.Split(' ')[1]
-                Address  = $ServerEndpoint.Address
+            if ($returnData) {
+                $output = [PSCustomObject]@{
+                    GameType = $returnData.Split(' ')[0]
+                    Port     = $returnData.Split(' ')[1]
+                    Address  = $ServerEndpoint.Address
+                }
+                $output | Write-Output
             }
-            $output | Write-Output
+        }
+    } catch {
+        if ($_.Exception.InnerException.ErrorCode -eq 10060) {
+            Write-Verbose "haven't heard anything for a while. Shutting the connection down. "
         }
     } finally {
         $udpClient.Close()
@@ -42,42 +50,47 @@ function Get-UtLanServerInfo {
         [int]
         $Port
     )
-    $encoding = [System.Text.AsciiEncoding]::new()
-
-    $respondentEndpoint = [IPEndpoint]::new([ipaddress]::Parse($Address), $Port)
-    
-    try {
-        $UtServerUtpClient = [System.Net.Sockets.UdpClient]::new($respondentEndpoint.Port)
-
-        $UtServerUtpClient.Connect($respondentEndpoint)
-
-        [Byte[]] $sendInfoBytes = $encoding.GetBytes("\info\");
-        $UtServerUtpClient.Send($sendInfoBytes, $sendInfoBytes.Length)
-
-        [Byte[]] $receiveBytes = $UtServerUtpClient.Receive([ref]$respondentEndpoint)
-        $returnData = $encoding.GetString($receiveBytes) 
-
-    } finally {
-        $UtServerUtpClient.Close()
+    begin {
+        $encoding = [System.Text.AsciiEncoding]::new()
     }
+    process {
 
-    <#
+        $respondentEndpoint = [IPEndpoint]::new([ipaddress]::Parse($Address), $Port)
+    
+        try {
+            $UtServerUtpClient = [System.Net.Sockets.UdpClient]::new($respondentEndpoint.Port)
+
+            $UtServerUtpClient.Connect($respondentEndpoint)
+
+            [Byte[]] $sendInfoBytes = $encoding.GetBytes("\info\");
+            $UtServerUtpClient.Send($sendInfoBytes, $sendInfoBytes.Length) | Out-Null
+
+            [Byte[]] $receiveBytes = $UtServerUtpClient.Receive([ref]$respondentEndpoint)
+            $returnData = $encoding.GetString($receiveBytes) 
+
+        } finally {
+            $UtServerUtpClient.Close()
+        }
+
+        <#
     The response from the server is something like:
     \hostname\UT Server In Docker\hostport\7777\maptitle\HiSpeed...
     It's essentially a key value pair in string format.
     So we split it into separate objects and order them as a hash table. 
     The hash is converted into a pscustomobject for that powershell feeling. 
     #>
-    $splitPath = $returnData.Split('\', [StringSplitOptions]::RemoveEmptyEntries)
-    $hash = @{}
-    for ($i = 0; $i -lt $splitPath.Count; $i++) {
-        $hash[$splitPath[$i]] = $splitPath[$i + 1]
-        $i++
+        $splitPath = $returnData.Split('\', [StringSplitOptions]::RemoveEmptyEntries)
+        $hash = @{}
+        for ($i = 0; $i -lt $splitPath.Count; $i++) {
+            $hash[$splitPath[$i]] = $splitPath[$i + 1]
+            $i++
+        }
+        $hash.Remove('final')
+        $hash['hostaddress'] = $respondentEndpoint.Address
+        [PSCustomObject]$hash | Write-Output
     }
-    $hash['hostaddress'] = $respondentEndpoint.Address
-    [PSCustomObject]$hash | Write-Output
+    end {}
 }
-#endregion
 
 
 
