935d4743eb3f9a858004672f5f371acfb6604695
Kyle Smith <Kyle.Smith1@Nintex.com>
2020-10-17 11:07:47 +1100

    Updated comments Cleaned up code Added host output if the security challenge isn't wookie

--- src/Get-UtMasterServerIpAddressList.ps1
+++ src/Get-UtMasterServerIpAddressList.ps1
@@ -1,27 +1,75 @@
 function Get-UtMasterServerEndpointList {
     <#
     .SYNOPSIS
-        Gets a list of Ip Addresses and port numbers from Master Servers.
+        Retrieves Endpoints from Master Servers.
     .DESCRIPTION
-        Long description
+        Retrieves a list of IP Endpoints from master servers. It's useful for testing if your UT99 game server is successfully announcing its presense to a master server. 
+        
+        When a UT Game server is started it might say:
+        UdpServerUplink: Master Server is master.mplayer.com:27900
+        UdpServerUplink: Port 7779 successfully bound.
+        However, this just means that it was able to a) resolve the dns host name to an ip address and b) bind to the port 7779 on the system. 
+        It does not mean the game server successfully announced its presense to that master server. 
+
+        Another problem is the UT99 Game client, will only show a list of servers in the browser that it managed to ping, 
+        so, you will not know if the problem is between the client and the game server, or the game server and the master server.
+
+        This cmdlet allows you to test two things
+        1. The master server address and port number are responding to connections.
+        2. The list of IP addresses is retrievable and your game server's ip address is on that list.
+
     .EXAMPLE
-        PS C:\> <example usage>
-        Explanation of what the example does
+        PS C:\> Get-UtMasterServerEndpointList -Address utmaster.epicgames.com 
+        AddressFamily Address          Port
+        ------------- -------          ----
+        InterNetwork 147.135.23.65    7978
+        InterNetwork 216.155.140.138  7778
+        InterNetwork 213.230.216.2    8889
+        InterNetwork 85.14.229.240    7778
+        InterNetwork 5.9.21.239       8076
+
+
+        Providing only the required parameters will return the list of endpoints
+
+    .EXAMPLE
+        PS C:\> Get-UtMasterServerEndpointList -Address master.333networks.com
+        
+        The challenge received from the server was \basic\\secure\HZVXFR\final\.  
+        This cmdlet can't handle any key other than the one used by epic servers.
+
+        In this example, the address for a master server was given where the challenge was outside of the capabilities of this cmdlet (for now).
+        However, it did respond with a challenge which is shown in the host output, which means the server is there and responding to connections.
+        This is a way you can test master servers to see if they are responding to requests. 
+    .EXAMPLE
+        PS C:\> Get-UtMasterServerEndpointList -Address unreal.epicgames.com | where Address -eq '181.43.152.180'
+        
+        AddressFamily Address        Port
+        ------------- -------        ----
+        InterNetwork 181.43.152.180 8201
+        InterNetwork 181.43.152.180 8301
+        InterNetwork 181.43.152.180 8401
+        InterNetwork 181.43.152.180 7778
+        InterNetwork 181.43.152.180 8305
+
+        In this example, the endpoints are filtered to only show Endpoints where the ip address equals '181.43.152.180'.
     .INPUTS
-        Inputs (if any)
+        string
     .OUTPUTS
-        Output (if any)
+        object[] or System.Net.IPEndpoint
     .NOTES
-        General notes
+        Known issues:
+            - The cmdlet will only download IPEndpoints from servers that use the challenge '\basic\\secure\wookie' 
+    .LINK
+        https://github.com/RIKIKU/UT99-Tools
     #>
     
     [cmdletbinding()]
     param( 
-        # IP address or hostname
+        # IP address or hostname of the master server
         [Parameter(Mandatory = $true)]
         [string]
         $Address,
-        # Port number to connec to the server on. Usually 28900
+        # Port number to connect to the server on. Default 28900
         [Parameter(Mandatory = $false)]
         [int]
         $Port = 28900 
@@ -80,18 +128,21 @@ function Get-UtMasterServerEndpointList {
      
             while ($stream.DataAvailable) {
                 $read = $stream.Read( $buffer, 0, 1024 )
-                $SecurityKeyResponse = $encoding.GetString( $buffer, 0, $read )
-                Write-Verbose "Security Key $SecurityKeyResponse"
+                $SecurityChallenge = $encoding.GetString( $buffer, 0, $read )
+                Write-Verbose "Security Challenge $SecurityChallenge"
             }
 
-            <# while ($stream.CanWrite -eq $false) {
-            Write-Verbose "waiting for stream to be writeable"
-            Start-Sleep -m 10
-        } #>
+            #One time I had to wait for the stream to become writable. 
+            while ($stream.CanWrite -eq $false) {
+                Write-Verbose "waiting for stream to be writeable"
+                Start-Sleep -m 10
+            }
         
-            #If the security key is 'wookie', we know the validation of that key we have to send back is '2/TYFMRc'
-            #Implementing gsmsalg is how you would get the correct validation for different keys, but I won't implement that here.
-            if ($SecurityKeyResponse -eq '\basic\\secure\wookie') {
+            <#
+            If the security challenge is 'wookie', we know the validation of that challenge we have to send back is '2/TYFMRc'
+            Implementing gsmsalg is how you would get the correct validation for different keys.
+            #>
+            if ($SecurityChallenge -eq '\basic\\secure\wookie') {
             
                 Write-Verbose "Sending request for ip address list"
                 $writer.WriteLine("\gamename\ut\location\0\validate\2/TYFMRc\final\\list\gamename\ut\")
@@ -110,10 +161,13 @@ function Get-UtMasterServerEndpointList {
                         Start-Sleep -m 10
                     }
                 }
+                Write-Verbose "Processing Ip Address List."
+                $IpString = $IpString.ToString().Split('\final')[0]
+                $IpString.Split('\ip\', [StringSplitOptions]::RemoveEmptyEntries).ForEach( { [IPEndpoint]::Parse($_) | Write-Output })
+            } else {
+                Write-Host "The challenge received from the server was $SecurityChallenge`nThis cmdlet can't handle any challenge other than the one used by epic servers."
             }
-            Write-Verbose "Processing Ip Address List."
-            $IpString = $IpString.ToString().Split('\final')[0]
-            $IpString.Split('\ip\', [StringSplitOptions]::RemoveEmptyEntries).ForEach( { [IPEndpoint]::Parse($_) | Write-Output })
+            
         
     
         } finally {
@@ -123,15 +177,3 @@ function Get-UtMasterServerEndpointList {
     }
     end {}
 }
-
-<# Write-Host "Started Query Run At $(get-date)"
-do {
-    $results = Get-UtMasterServerEndpointList -Address utmaster.epicgames.com
-    $ImHere = $results | where Address -EQ '121.200.8.51'
-    Start-Sleep -Seconds 30 
-} while ($ImHere) 
-
-get-date #>
-
-
-Get-UtMasterServerEndpointList -Address unreal.epicgames.com -Verbose # | where Address -EQ '121.200.8.51'
