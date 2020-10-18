dc2dde126e59b2fe07575540c26ff5fc154ff114
Kyle Smith <Kyle.Smith1@Nintex.com>
2020-10-18 18:18:08 +1100

    Module name change

--- build/build.ps1
+++ build/build.ps1
@@ -1,7 +1,7 @@
-$ModuleName = 'ut99tools'
+$ModuleName = 'Ut99Tools'
 $SourceFolder = ($PSScriptRoot | Split-Path) + '\src'
 $OutputPath = ($PSScriptRoot | Split-Path) + "\staging\$ModuleName"
-#Remove-Item -Path $OutputPath -Force -ErrorAction SilentlyContinue
+Remove-Item -Path $OutputPath -Force -ErrorAction SilentlyContinue
 New-Item -ItemType Directory -Path $OutputPath -Force | Out-Null 
 Get-ChildItem -Path "$SourceFolder\*.ps1" -Recurse | Get-Content | Out-File -FilePath "$OutputPath\$ModuleName.psm1"
 Copy-Item "$SourceFolder\*" -Recurse -Exclude '*.ps1' -Destination $OutputPath
